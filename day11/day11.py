e99cb4694cb6be2234cc4d9b54dfcbc46bf31241
David Williams <davwil00@gmail.com>
2019-01-20 21:49:10 +0000

    day 11 - part 2 wip

--- day11/day11.py
+++ day11/day11.py
@@ -2,6 +2,7 @@ from collections import namedtuple
 from itertools import chain
 
 GRID_SERIAL_NUMBER = 8444
+MAX_SQUARE_SIZE = 300
 # GRID_SERIAL_NUMBER = 42
 
 Coordinate = namedtuple("Coordinate", ["x", "y"])
@@ -18,11 +19,11 @@ def calculate_power_level(x_coord, y_coord):
     return power_level
 
 
-def create_square(top_left):
+def create_square(top_left, square_size):
     grid = []
-    for y in range(top_left.y, top_left.y + 3):
+    for y in range(top_left.y, top_left.y + square_size):
         row = []
-        for x in range(top_left.x, top_left.x + 3):
+        for x in range(top_left.x, top_left.x + square_size):
             row.append(calculate_power_level(x, y))
         grid.append(row)
     return grid
@@ -34,21 +35,30 @@ def calculate_grid_sum(grid):
 
 def run():
     max_power = 0
-    max_power_coords = Coordinate(0,0)
-    for x in range(1, 298):
-        for y in range(1, 298):
-            coordinates = Coordinate(x, y)
-            grid = create_square(coordinates)
-            total_power = calculate_grid_sum(grid)
-            if total_power > max_power:
-                max_power = total_power
-                max_power_coords = coordinates
+    max_power_coords = Coordinate(0, 0)
+    grid = create_square(Coordinate(1,1), MAX_SQUARE_SIZE)
+    for square_size in range(280, MAX_SQUARE_SIZE):
+        print(square_size)
+        for x in range(1, MAX_SQUARE_SIZE - square_size + 1):
+            for y in range(1, MAX_SQUARE_SIZE - square_size + 1):
+                max_power, max_power_coords = find_subgrid_power(grid, max_power, max_power_coords, square_size, x, y)
     print(max_power_coords)
-    print(total_power)
+    print(max_power)
 
 
-grid = create_square(Coordinate(21, 61))
-[print(row) for row in grid]
-print(calculate_grid_sum(grid))
+def find_subgrid_power(grid, max_power, max_power_coords, square_size, x, y):
+    coordinates = Coordinate(x, y)
+    subgrid = [grid[y + i - 1][x - 1:x + square_size - 1] for i in range(square_size)]
+    # subgrid = create_square(Coordinate(x, y), square_size)
+    total_power = calculate_grid_sum(subgrid)
+    if total_power > max_power:
+        max_power = total_power
+        max_power_coords = coordinates
+    return max_power, max_power_coords
+
+
+# grid = create_square(Coordinate(21, 61))
+# [print(row) for row in grid]
+# print(calculate_grid_sum(grid))
 
 run()
